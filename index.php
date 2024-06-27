3a51064027a05822bb876babe89f02d3d75ef77d
omerov27 <145061108+omerov27@users.noreply.github.com>
2024-06-27 15:57:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+include("code/yonet.php");
+include("code/connect.php");
+?>
+<!doctype html>
+<html lang="tr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Fenerbahçe</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="stils.css">
+</head>
+
+<body>
+  <div id="container">
+    <div id="header">
+      <div id="menu">
+        <ul>
+          <hr width="1200px">
+          <li><a href="index.php">Ansayfa</a></li>
+          <li><a href="https://fenerium.com/" target="_blank">Fenerium</a></li>
+          <li><a href="code/kulup.php">KULÜP</a></li>
+          <li class="brans-li"><a href="code/yonetim.php">Yönetim </a> </li>
+          <li><a href="code/oyuncu.php"> Oyuncularımız</a></li>
+          <li><a href="code/teknik.php">Teknik Heyet</a></li>
+          <li><a href="code/fenerbahceAdmin/index.php" target="_blank">Admin Sayfası</a></li>
+          <hr width="1200px">
+
+        </ul>
+        <br>
+      </div>
+    </div>
+    <div style="margin-bottom:5px; ">
+      <img src="imgs/banneruygulamafbsk.jpg" class="resim1" , width="auto" height="560" alt="">
+    </div>
+    <div id="content">
+      <div style="padding-left: 5px; margin-left: 5px;" id="content-left">
+        <h1>ŞAMPİYON FENERBAHÇE BEKO</h1>
+        <img src="imgs/ozelden-fenerbahce-bekoya-kutlama-mesaji.jpg" style="margin-top: 20px;" width="530px" alt="">
+        <p>Fenerbahçe Beko Erkek Basketbol Takımımız, Türkiye Kupası şampiyonluğunun ardından ligi de şampiyonlukla tamamladı.</p>
+        <p>Fenerbahçe Beko, Türkiye Sigorta Basketbol Süper Ligi play-off final serisi dördüncü maçında Anadolu Efes’i konuk etti. Normal süresi 64-64 eşitlikle tamamlanan maçta rakibini uzatma periyodu sonunda 80-72 mağlup eden Fenerbahçe Beko, seriyi 3-1’e getirdi ve şampiyonluğunu ilan etti.</p>
+    </div>
+      <div id="content-left">
+        <h1>Jose Mourinho İstanbul’da</h1>
+        <img src="imgs/mourinhokpk.jpg" style="margin-top: 20px;" width="530px" alt="">
+        <p>Kulübümüzün, Futbol A Takımımızın teknik direktörlüğü için görüşmelere başladığını duyurduğu dünyaca ünlü Portekizli teknik adam Jose Mourinho İstanbul’a geldi.</p>
+        <p>İngiltere’den kalkan özel uçakla İstanbul’a gelen Jose Mourinho’ya yolculuk esnasında Başkanımız Ali Y. Koç, Sayın Acun Ilıcalı ve Sportif Direktörümüz Mario Branco da eşlik etti.</p>
+      </div>
+    </div>
+  </div>
+  <hr size="15px" style="color: blue; ">
+  <footer class="site-footer">
+      <div class=" container">
+    <div class="row">
+
+      <div class="col-xs-6 col-md-3">
+        <h1>Haberler</h1>
+        <ul class="footer-links">
+          <li><a href="https://www.fenerbahce.org/Haberler?hb=club">Kulüp</a></li>
+          <li><a href="https://www.fenerbahce.org/Haberler?hb=football">Futbol</a></li>
+          <li><a href="https://www.fenerbahce.org/Haberler?hb=basketball-man">Basketbol Erkek</a></li>
+          <li><a href="https://www.fenerbahce.org/Haberler?hb=basketball-woman">Basketbol Kadın</a></li>
+          <li><a href="https://www.fenerbahce.org/Haberler?hb=volleyball-man">Voleybol Erkek</a></li>
+          <li><a href="https://www.fenerbahce.org/Haberler?hb=volleyball-woman">Voleybol Kadın</a></li>
+        </ul>
+      </div>
+
+      <div class="col-xs-6 col-md-3">
+        <h1>Kulüp</h1>
+        <ul class="footer-links">
+          <li><a href="https://www.fenerbahce.org/Kulup/Fenerbahce-SK">Fenerbahçe SK</a></li>
+          <li><a href="https://www.fenerbahce.org/Kulup/Tarihce">Tarihçe</a></li>
+          <li><a href="https://www.fenerbahce.org/muze">Müze</a></li>
+          <li><a href="https://www.fenerbahce.org/Kulup/Yonetim-Kurulu">Yönetim Kurulu</a></li>
+          <li><a href="https://www.fenerbahce.org/Kulup/Ataturk-Fenerbahce">Atatürk ve Fenerbahçe</a></li>
+        </ul>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <h1>Görüşlerin bizim için çok önemli</h1>
+        <br>
+        <h5>Görüşlerini paylaşmak için <a href="code/gorus.php" target="_blank">Tıkla</a></h5>
+      </div>
+    </div>
+
+    <hr>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; Fenerbahçe SK
+          </p>
+        </div>
+
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li><a class="facebook" href="https://github.com/omerov27" target="_blank"><i class="bi bi-github"></i></a></li>
+            <li><a class="twitter" href="https://x.com/omerruren" target="_blank"><i class="bi bi-twitter"></i></a></li>
+            <li><a class="dribbble" href="https://www.instagram.com/omeruren27/" target="_blank"><i class="bi bi-instagram"></i></i></a></li>
+            <li><a class="linkedin" href="https://www.linkedin.com/in/%C3%B6mer-%C3%BCren-2b9542315/" target="_blank"><i class="bi bi-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
+<?php
+mysqli_close($con);
+?>
